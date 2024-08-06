245bfe8e44ac1686a9065b357a550e74d42be352
Beyley Thomas <ep1cm1n10n123@gmail.com>
2024-08-05 21:28:33 -0700

    migrate

---
+++ GemBooru/Migrations/20240806042519_InitialCreate.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using GemBooru.Database;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace GemBooru.Migrations
+{
+    /// <inheritdoc />
+    [DbContext(typeof(GemBooruDatabaseContext))]
+    [Migration("20240806042519_InitialCreate")]
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CertificateHash = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    Name = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    Bio = table.Column<string>(type: "character varying(4096)", maxLength: 4096, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Width = table.Column<int>(type: "integer", nullable: false),
+                    Height = table.Column<int>(type: "integer", nullable: false),
+                    FileSizeInBytes = table.Column<int>(type: "integer", nullable: false),
+                    Source = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    UploaderId = table.Column<int>(type: "integer", nullable: false),
+                    UploadDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    PostType = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Posts_Users_UploaderId",
+                        column: x => x.UploaderId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TagRelations",
+                columns: table => new
+                {
+                    TagRelationId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PostId = table.Column<int>(type: "integer", nullable: false),
+                    Tag = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TagRelations", x => x.TagRelationId);
+                    table.ForeignKey(
+                        name: "FK_TagRelations_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "PostId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_UploaderId",
+                table: "Posts",
+                column: "UploaderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TagRelations_PostId",
+                table: "TagRelations",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TagRelations_Tag_PostId",
+                table: "TagRelations",
+                columns: new[] { "Tag", "PostId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TagRelations");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
